d7e45f2511214b588dd01a384d1ffefe2a9aacf5
Timfreaky <timmcmonney@hotmail.com>
2015-02-19 17:36:00 +0100

    1st commit

---
+++ maventest1/src/maventest1/Main.java
@@ -0,0 +1,13 @@
+package maventest1;
+
+import javax.mail.event.MailEvent;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		MailEvent mailEvent1 = null;
+
+	}
+
+}
