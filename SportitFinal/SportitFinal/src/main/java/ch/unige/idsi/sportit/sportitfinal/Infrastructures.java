6c1f6ff442d5899579c8033f6d8bbdd9deb1ac30
florinem <florine-monnier@hotmail.com>
2015-05-07 16:57:00 +0200

    1rst commit

---
+++ SportitFinal/SportitFinal/src/main/java/ch/unige/idsi/sportit/sportitfinal/Infrastructures.java
@@ -0,0 +1,22 @@
+package ch.unige.idsi.sportit.sportitfinal;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class Infrastructures {
+
+    /**
+     */
+    private String name;
+
+    /**
+     */
+    private String latitude;
+
+    /**
+     */
+    private String longitude;
+}
