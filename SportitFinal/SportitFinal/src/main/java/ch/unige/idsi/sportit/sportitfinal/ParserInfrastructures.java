bcd51868fae13908f5bf4267a29af0da6be4af0c
florinem <florine-monnier@hotmail.com>
2015-05-08 12:06:41 +0200

    with parse

--- SportitFinal/SportitFinal/src/main/java/ch/unige/idsi/sportit/sportitfinal/ParserInfrastructures.java
+++ SportitFinal/SportitFinal/src/main/java/ch/unige/idsi/sportit/sportitfinal/ParserInfrastructures.java
@@ -1,7 +1,15 @@
 package ch.unige.idsi.sportit.sportitfinal;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -11,7 +19,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.transaction.annotation.Transactional;
 
-import ch.unige.idsi.sportit.sportitfinal.Infrastructures;
 import de.micromata.opengis.kml.v_2_2_0.Coordinate;
 import de.micromata.opengis.kml.v_2_2_0.Document;
 import de.micromata.opengis.kml.v_2_2_0.Feature;
@@ -40,13 +47,29 @@ public class ParserInfrastructures extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 System.out.println("process start...");
-		
-		final Kml kml = Kml.unmarshal(new File("http://sportitfinal.cfapps.io/infrastructures.kml"));
-		System.out.println("process run");
-		// System.out.println("process run2");
-		final Document document = (Document) kml.getFeature();
-		System.out.println(document.getName());
 
+	
+		URL urlkml = new URL("http://sportitfinal.cfapps.io/infrastructures.kml");
+		BufferedReader in = new BufferedReader(
+		new InputStreamReader(urlkml.openStream()));
+		String inputLine;
+		File file = new File("kmlFile.kml");
+		FileWriter fw = new FileWriter(file.getAbsoluteFile());
+		BufferedWriter bw = new BufferedWriter(fw);
+		
+	        while ((inputLine = in.readLine()) != null){
+	            System.out.println(inputLine);
+	            bw.write(inputLine);
+	        }
+	        in.close();
+	        bw.close();
+	    
+	        final Kml kml = Kml.unmarshal("kmlFile.kml");
+			System.out.println("process run");
+			final Document document = (Document) kml.getFeature();
+			System.out.println(document.getName());
+	
+	        
 		List<Feature> t = document.getFeature();
         for(Object o : t){
             Folder f = (Folder)o;
