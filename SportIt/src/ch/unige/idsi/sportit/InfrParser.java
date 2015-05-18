eb82a47811751201a9483563f61366843e201873
Timfreaky <timmcmonney@hotmail.com>
2015-05-18 08:56:55 +0200

    1st javadoc commit

--- SportIt/src/ch/unige/idsi/sportit/InfrParser.java
+++ SportIt/src/ch/unige/idsi/sportit/InfrParser.java
@@ -16,87 +16,90 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.parser.Parser;
 
-
 import com.google.android.gms.maps.model.LatLng;
 
 public class InfrParser {
-	
+
 	public InfrParser() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
 	public ArrayList<LatLng> getCoordinateArrays(InputStream stream) {
-	    ArrayList<LatLng> onePlace = new ArrayList<LatLng>();
+		ArrayList<LatLng> onePlace = new ArrayList<LatLng>();
 
-	    try {
-	        StringBuilder buff = new StringBuilder();
-	        
-	        InputStream json = stream;
-	        BufferedReader in = new BufferedReader(new InputStreamReader(json));
-	        String string;
-	                      String buffer;
-	        while ((string = in.readLine()) != null) {
-	            buff.append(string);
-	        }
+		try {
+			StringBuilder buff = new StringBuilder();
 
-	        in.close();
-	        String html = buff.toString();
-	        Document doc = Jsoup.parse(html, "", Parser.xmlParser());
-	        ArrayList<String> tracksString = new ArrayList<String>();
+			InputStream json = stream;
+			BufferedReader in = new BufferedReader(new InputStreamReader(json));
+			String string;
+			String buffer;
+			while ((string = in.readLine()) != null) {
+				buff.append(string);
+			}
 
-	        for (Element e : doc.select("coordinates")) {
-	            tracksString.add(e.toString().replace("<coordinates>", "").replace("</coordinates>", ""));
-	        }
+			in.close();
+			String html = buff.toString();
+			Document doc = Jsoup.parse(html, "", Parser.xmlParser());
+			ArrayList<String> tracksString = new ArrayList<String>();
 
-	        for (int i = 0; i < tracksString.size(); i++) {
-	          
-	            ArrayList<String> onePlaceString = new ArrayList<String>(Arrays.asList(tracksString.get(i).split("\\s+")));
-	            for (int k = 1; k < onePlaceString.size(); k++) {
-	                LatLng latLng = new LatLng(Double.parseDouble(onePlaceString.get(k).split(",")[0]),
-	                        Double.parseDouble(onePlaceString.get(k).split(",")[1]));
-	                onePlace.add(latLng);
-	            }
-	            
-	        }
+			for (Element e : doc.select("coordinates")) {
+				tracksString.add(e.toString().replace("<coordinates>", "")
+						.replace("</coordinates>", ""));
+			}
 
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    }
-	    System.out.println(onePlace);
-	    return onePlace;
-	    
+			for (int i = 0; i < tracksString.size(); i++) {
+
+				ArrayList<String> onePlaceString = new ArrayList<String>(
+						Arrays.asList(tracksString.get(i).split("\\s+")));
+				for (int k = 1; k < onePlaceString.size(); k++) {
+					LatLng latLng = new LatLng(
+							Double.parseDouble(onePlaceString.get(k).split(",")[0]),
+							Double.parseDouble(onePlaceString.get(k).split(",")[1]));
+					onePlace.add(latLng);
+				}
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(onePlace);
+		return onePlace;
+
 	}
+
 	public ArrayList<String> getNamesArrays(InputStream stream) {
-		 ArrayList<String> namesString = new ArrayList<String>();
-		 try {
-		        StringBuilder buffe = new StringBuilder();
-		        
-		        InputStream json = stream;
-		        BufferedReader in = new BufferedReader(new InputStreamReader(json));
-		        String string;
-		        String buffer;
-		        while ((string = in.readLine()) != null) {
-		            buffe.append(string);
-		        }
+		ArrayList<String> namesString = new ArrayList<String>();
+		try {
+			StringBuilder buffe = new StringBuilder();
 
-		        in.close();
-		        String html = buffe.toString();
-		        Document doc = Jsoup.parse(html, "", Parser.xmlParser());
-		       
-		        for (Element e : doc.select("SimpleData")) {
-		        	if(e.attr("name").equalsIgnoreCase("TYPE")){
-		            namesString.add(e.toString().replace("<simpledata name=\"TYPE\">", "").replace("</simpledata>", ""));
-		        	}
-		        }
-		 } catch (Exception f) 
-		 	{
-		        f.printStackTrace();
-		    }
-		
+			InputStream json = stream;
+			BufferedReader in = new BufferedReader(new InputStreamReader(json));
+			String string;
+			String buffer;
+			while ((string = in.readLine()) != null) {
+				buffe.append(string);
+			}
+
+			in.close();
+			String html = buffe.toString();
+			Document doc = Jsoup.parse(html, "", Parser.xmlParser());
+
+			for (Element e : doc.select("SimpleData")) {
+				if (e.attr("name").equalsIgnoreCase("TYPE")) {
+					namesString.add(e.toString()
+							.replace("<simpledata name=\"TYPE\">", "")
+							.replace("</simpledata>", ""));
+				}
+			}
+		} catch (Exception f) {
+			f.printStackTrace();
+		}
+
 		return namesString;
-		 
-	}
-	//http://sporitfinal.cfapps.io/doc_inf.kml
-}
 
+	}
+
+}
--- SportIt/src/ch/unige/idsi/sportit/PathParser.java
+++ SportIt/src/ch/unige/idsi/sportit/PathParser.java
@@ -16,12 +16,9 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.parser.Parser;
 
-
 import com.google.android.gms.maps.model.LatLng;
 
 public class PathParser {
-	
-	
 
 	public PathParser() {
 		super();
@@ -29,44 +26,47 @@ public class PathParser {
 	}
 
 	public ArrayList<ArrayList<LatLng>> getCoordinateArrays(InputStream stream) {
-	    ArrayList<ArrayList<LatLng>> allTracks = new ArrayList<ArrayList<LatLng>>();
+		ArrayList<ArrayList<LatLng>> allTracks = new ArrayList<ArrayList<LatLng>>();
 
-	    try {
-	        StringBuilder buf = new StringBuilder();
-	        
-	        InputStream json = stream;
-	        BufferedReader in = new BufferedReader(new InputStreamReader(json));
-	        String str;
-	                      String buffer;
-	        while ((str = in.readLine()) != null) {
-	            buf.append(str);
-	        }
+		try {
+			StringBuilder buf = new StringBuilder();
 
-	        in.close();
-	        String html = buf.toString();
-	        Document doc = Jsoup.parse(html, "", Parser.xmlParser());
-	        ArrayList<String> tracksString = new ArrayList<String>();
-	        for (Element e : doc.select("coordinates")) {
-	            tracksString.add(e.toString().replace("<coordinates>", "").replace("</coordinates>", ""));
-	        }
+			InputStream json = stream;
+			BufferedReader in = new BufferedReader(new InputStreamReader(json));
+			String str;
+			String buffer;
+			while ((str = in.readLine()) != null) {
+				buf.append(str);
+			}
 
-	        for (int i = 0; i < tracksString.size(); i++) {
-	            ArrayList<LatLng> oneTrack = new ArrayList<LatLng>();
-	            ArrayList<String> oneTrackString = new ArrayList<String>(Arrays.asList(tracksString.get(i).split("\\s+")));
-	            for (int k = 1; k < oneTrackString.size(); k++) {
-	                LatLng latLng = new LatLng(Double.parseDouble(oneTrackString.get(k).split(",")[0]),
-	                        Double.parseDouble(oneTrackString.get(k).split(",")[1]));
-	                oneTrack.add(latLng);
-	            }
-	            allTracks.add(oneTrack);
-	        }
+			in.close();
+			String html = buf.toString();
+			Document doc = Jsoup.parse(html, "", Parser.xmlParser());
+			ArrayList<String> tracksString = new ArrayList<String>();
+			for (Element e : doc.select("coordinates")) {
+				tracksString.add(e.toString().replace("<coordinates>", "")
+						.replace("</coordinates>", ""));
+			}
 
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    }
-	    //System.out.println(allTracks);
-	    return allTracks;
-	    
+			for (int i = 0; i < tracksString.size(); i++) {
+				ArrayList<LatLng> oneTrack = new ArrayList<LatLng>();
+				ArrayList<String> oneTrackString = new ArrayList<String>(
+						Arrays.asList(tracksString.get(i).split("\\s+")));
+				for (int k = 1; k < oneTrackString.size(); k++) {
+					LatLng latLng = new LatLng(
+							Double.parseDouble(oneTrackString.get(k).split(",")[0]),
+							Double.parseDouble(oneTrackString.get(k).split(",")[1]));
+					oneTrack.add(latLng);
+				}
+				allTracks.add(oneTrack);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		// System.out.println(allTracks);
+		return allTracks;
+
 	}
-	
+
 }
