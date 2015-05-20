93cb2e5edd231a37f1cdb4cd240649a5a0af5de3
florinem <florine-monnier@hotmail.com>
2015-05-20 15:33:43 +0200

    markers infrastructures ok

--- SportitFinal/SportitFinal/src/main/java/ch/unige/idsi/sportit/sportitfinal/MapsController.java
+++ SportitFinal/SportitFinal/src/main/java/ch/unige/idsi/sportit/sportitfinal/MapsController.java
@@ -1,9 +1,10 @@
 package ch.unige.idsi.sportit.sportitfinal;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+import java.util.Iterator;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -11,15 +12,44 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MapsController {
 
-    @RequestMapping(method = RequestMethod.POST, value = "{id}")
-    public void post(@PathVariable Long id, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
-    }
-
     @RequestMapping
+    //(method = RequestMethod.GET, value = "/map/index")
     public String index(final ModelMap model) {
-    	model.addAttribute("markersInfra",Infrastructures.findAllInfrastructureses());
-    	model.addAttribute("cheminsPedestres",Chemins.findAllCheminses());
+    	
+    	String markersInfra = "var infra = [";
+    	List<Infrastructures> infrastructures = Infrastructures.findAllInfrastructureses();
+		for (Iterator<Infrastructures> i = infrastructures.iterator(); i.hasNext();) {
+			Infrastructures infrastructure = (Infrastructures) i.next();
+			markersInfra += "['"+ infrastructure.getName() + "'," + infrastructure.getLatitude()+ "," + infrastructure.getLongitude()+"],";
+		}
+		markersInfra +="[' ',0,0]];";
+		
+		//System.out.println(markersInfra);
+		model.addAttribute("markerInfra", markersInfra);
+		
+		/*
+		String chemin = "";
+		List<Chemins> chemins = Chemins.findAllCheminses();
+			for (Iterator<Chemins> it = chemins.iterator(); it.hasNext();) {
+				Chemins chem = (Chemins) it.next();
+				
+				List<Double> lat = chem.getLatitude();
+				List<Double> lng = chem.getLongitude();
+				
+				chemin += "var chemLatLng = [";
+					for (int i=0; i<lat.size(); i++){
+						Double lati = lat.get(i);
+						Double longi = lng.get(i);
+						chemin += "new google.maps.LatLng("
+								+ lati
+								+ ","
+								+ longi
+								+")];";
+					}
+							}
+			
+    	model.addAttribute("cheminsPedestres",chemin);
+    	System.out.println(chemin);*/
         return "maps/index";
-
     }
 }
